de00fb9d870138514d834368606fc4cd9c9e0662
NinZZya <s.a.azarov@gmail.com>
2020-09-30 16:23:11 +0300

    Add layout for Main page

---
+++ src/pages/main/main.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import Header from '../../components/header';
+import Tabs from '../../components/tabs';
+import Sort from '../../components/sort';
+import OffersList from '../../components/offers-list/offers-list';
+import {CITIES} from '../../const';
+import {TCityId} from '../../types';
+
+const Main = ({activeCityId}) => {
+  return (
+    <div className="page page--gray page--main">
+      <Header />
+      <main className="page__main page__main--index">
+        <h1 className="visually-hidden">Cities</h1>
+        <Tabs activeCityId={activeCityId} />
+        <div className="cities">
+          <div className="cities__places-container container">
+            <section className="cities__places places">
+              <h2 className="visually-hidden">Places</h2>
+              <b className="places__found">
+                {`312 places to stay in ${CITIES[activeCityId].name}`}
+              </b>
+              <Sort />
+              <OffersList />
+            </section>
+            <div className="cities__right-section">
+              <section className="cities__map map"></section>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+Main.propTypes = {
+  activeCityId: TCityId,
+};
+
+export default Main;
