d358e8c59b1882112829341d336dff7f38411601
NinZZya <s.a.azarov@gmail.com>
2020-09-30 21:15:40 +0300

    Code review App

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import MainPage from '../../pages/main';
+import Main from '../../pages/main';
 import {TCityId} from '../../types';
 
 const App = ({activeCityId}) => {
+
   return (
-    <MainPage activeCityId={activeCityId} />
+    <Main activeCityId={activeCityId} />
   );
 };
 
