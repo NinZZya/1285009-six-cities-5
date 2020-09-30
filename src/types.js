65c800934442024378ff3250a3216a2eca1196db
NinZZya <s.a.azarov@gmail.com>
2020-09-30 21:03:42 +0300

    Add TStar type

--- src/types.js
+++ src/types.js
@@ -1,5 +1,7 @@
 import PropTypes from 'prop-types';
 
+const {number, shape, oneOf} = PropTypes;
+
 const CITIES = [
   `Paris`,
   `Cologne`,
@@ -9,8 +11,21 @@ const CITIES = [
   `Dusseldorf`,
 ];
 
-export const TCityId = PropTypes.number;
-export const TCity = PropTypes.shape({
+export const STAR_TITLES = [
+  `perfect`,
+  `good`,
+  `not bad`,
+  `badly`,
+  `terribly`,
+];
+
+export const TCityId = number;
+export const TCity = shape({
   id: TCityId,
-  name: PropTypes.oneOf(CITIES),
+  name: oneOf(CITIES),
+});
+
+export const TStar = shape({
+  value: number,
+  title: oneOf(STAR_TITLES),
 });
