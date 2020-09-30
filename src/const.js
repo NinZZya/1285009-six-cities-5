22a775862ff3b7476fa49d053d8e8b7d08d7256b
NinZZya <s.a.azarov@gmail.com>
2020-09-30 16:18:53 +0300

    Add constants

---
+++ src/const.js
@@ -0,0 +1,22 @@
+export const AppRoute = {
+  LOGIN: `/login`,
+  OFFER: `/offer/`,
+  FAVORITES: `/favorites`,
+  ROOT: `/`,
+};
+
+export const CITIES = {
+  1: {id: 1, name: `Paris`},
+  2: {id: 2, name: `Cologne`},
+  3: {id: 3, name: `Brussels`},
+  4: {id: 4, name: `Amsterdam`},
+  5: {id: 5, name: `Hamburg`},
+  6: {id: 6, name: `Dusseldorf`},
+};
+
+export const SortType = {
+  POPULAR: `Popular`,
+  LOW_TO_HIGH: `Price: low to high`,
+  HIGH_TO_LOW: `Price: high to low`,
+  TOP_RATED_FIRST: `Top rated first`,
+};
