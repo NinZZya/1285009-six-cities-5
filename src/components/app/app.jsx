0bbd782288fac52ff208bb0233df63610cb7e305
NinZZya <s.a.azarov@gmail.com>
2020-10-01 20:02:18 +0300

    Create routing in App

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
 import Main from '../../pages/main/main';
+import Login from '../../pages/login/login';
+import Offer from '../../pages/offer/offer';
+import Favorites from '../../pages/favorites/favorites';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
+import {AppRoute} from '../../const';
 import {TCityId} from '../../types';
+
 
 const App = ({activeCityId}) => {
   return (
-    <Main activeCityId={activeCityId} />
+    <Router>
+      <Switch>
+        <Route exact path={AppRoute.ROOT}>
+          <Main activeCityId={activeCityId} />
+        </Route>
+        <Route exact path={AppRoute.LOGIN} component={Login} />
+        <Route exact path={AppRoute.OFFER} component={Offer} />
+        <Route exact path={AppRoute.FAVORITES} component={Favorites} />
+        <Redirect to={AppRoute.ROOT} />
+      </Switch>
+    </Router>
   );
 };
 
