5d4e41bc536e4cb2f91c7c0f9b0152eb04ce2a13
NinZZya <s.a.azarov@gmail.com>
2020-10-05 21:10:18 +0300

    WIP. Review Main page

--- src/pages/main/main.jsx
+++ src/pages/main/main.jsx
@@ -1,29 +1,66 @@
 import React from 'react';
-import Tabs from '../../components/tabs/tabs';
+import CitiesTabs from '../../components/cities-tabs/cities-tabs';
 import Sort from '../../components/sort/sort';
 import OffersList from '../../components/offers-list/offers-list';
+import NoOffers from '../../components/no-offers/no-offers';
 import Map from '../../components/map/map';
-import {CITIES} from '../../const';
+import {CITIES, SortType} from '../../const';
 import * as Type from '../../types';
 
-const Main = ({activeCityId}) => {
+
+const OFFERS = new Array(5).fill(``);
+const DEFAULT_SORT = SortType.POPULAR;
+const SORTS = Object.values(SortType);
+
+const ClassName = {
+  PAGE_EMPTY: `page__main--index-empty`,
+  CONTAINER_EMPTY: `cities__places-container--empty`,
+  OFFERS_LIST: `cities__places-list tabs__content`,
+};
+
+const renderOffers = (offers, city) => {
   return (
-    <main className="page__main page__main--index">
+    <div className="cities__places-container container">
+      <section className="cities__places places">
+        <h2 className="visually-hidden">Places</h2>
+        <b className="places__found">
+          {`${offers.length} places to stay in ${city.name}`}
+        </b>
+        <Sort activeSort={DEFAULT_SORT} sorts={SORTS} />
+        <OffersList
+          offers={OFFERS}
+          className={ClassName.OFFERS_LIST}
+        />
+      </section>
+      <div className="cities__right-section">
+        <Map />
+      </div>
+    </div>
+  );
+};
+
+const Main = ({activeCityId}) => {
+  const activeCity = CITIES[activeCityId];
+
+  return (
+    <main
+      className={`page__main page__main--index ${
+        !OFFERS.length ? ClassName.PAGE_EMPTY : ``
+      }`}
+    >
       <h1 className="visually-hidden">Cities</h1>
-      <Tabs activeCityId={activeCityId} />
+      <CitiesTabs
+        activeCityId={activeCityId}
+      />
       <div className="cities">
-        <div className="cities__places-container container">
-          <section className="cities__places places">
-            <h2 className="visually-hidden">Places</h2>
-            <b className="places__found">
-              {`312 places to stay in ${CITIES[activeCityId].name}`}
-            </b>
-            <Sort />
-            <OffersList />
-          </section>
-          <div className="cities__right-section">
-            <Map />
-          </div>
+        <div
+          className={`cities__places-container container ${
+            !OFFERS.length ? ClassName.CONTAINER_EMPTY : ``
+          }`}
+        >
+          {OFFERS.length ?
+            renderOffers(OFFERS, activeCity) :
+            <NoOffers city={activeCity} />}
         </div>
       </div>
     </main>
@@ -31,7 +68,8 @@ const Main = ({activeCityId}) => {
 };
 
 Main.propTypes = {
-  activeCityId: Type.id,
+  activeCityId: Type.ID,
 };
+
 
 export default Main;
