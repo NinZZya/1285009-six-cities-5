bf0c85c740bc96ca5f18f08b11d91cd78ee6ad45
NinZZya <s.a.azarov@gmail.com>
2020-10-05 21:06:27 +0300

    WIP. Review NoOffers

--- src/components/no-offers/no-offers.jsx
+++ src/components/no-offers/no-offers.jsx
@@ -1,22 +1,26 @@
 import React from 'react';
-import {CITIES} from '../../const';
-import {TCityId} from '../../types';
+import * as Type from '../../types';
 
-const NoOffers = ({activeCityId}) => {
+
+const NoOffers = ({city}) => {
   return (
-    <section className="cities__no-places">
-      <div className="cities__status-wrapper tabs__content">
-        <b className="cities__status">No places to stay available</b>
-        <p className="cities__status-description">
-          {`We could not find any property available at the moment in ${CITIES[activeCityId].name}`}
-        </p>
-      </div>
-    </section>
+    <>
+      <section className="cities__no-places">
+        <div className="cities__status-wrapper tabs__content">
+          <b className="cities__status">No places to stay available</b>
+          <p className="cities__status-description">
+            {`We could not find any property available at the moment in ${city.name}`}
+          </p>
+        </div>
+      </section>
+      <div className="cities__right-section"></div>
+    </>
   );
 };
 
 NoOffers.propTypes = {
-  activeCityId: TCityId
+  city: Type.CITY,
 };
+
 
 export default NoOffers;
