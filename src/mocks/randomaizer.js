665d948e9fb2a6da188db5825d2609f30407b022
NinZZya <s.a.azarov@gmail.com>
2020-10-06 19:21:16 +0300

    Bug fix in mocks

--- src/mocks/randomaizer.js
+++ src/mocks/randomaizer.js
@@ -1,4 +1,4 @@
-export const getRandomBool = () => Math.random > 0.5;
+export const getRandomBool = () => Math.random() >= 0.5;
 export const getRandomInt = (min, max) => min + Math.floor(Math.random() * (max - min));
 export const getRandomArrValue = (arr) => arr[Math.floor(Math.random() * arr.length)];
 export const getRandomArr = (arr, length = arr.length) => {
