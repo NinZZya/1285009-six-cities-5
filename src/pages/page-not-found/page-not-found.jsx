4835c6fa6921c56c2e3f28e9855de82c40cde4ad
NinZZya <s.a.azarov@gmail.com>
2020-10-06 09:32:32 +0300

    Style review PageNotFound page

--- src/pages/page-not-found/page-not-found.jsx
+++ src/pages/page-not-found/page-not-found.jsx
@@ -5,21 +5,23 @@ import {AppPath} from '../../const';
 
 const PageNotFound = () => {
   return (
-    <main className="page__main page__main--login">
-      <div className="page__login-container container">
-        <div className="login">
-          <h1 className="login__title">Page not found</h1>
-          <p>
-            Click button to visit the main page
-          </p>
-          <p>
-            <Link to={AppPath.ROOT} className="button form__submit">
-              Go to the main page
-            </Link>
-          </p>
+    <main className="page__main page__main--index page__main--index-empty">
+      <div className="cities">
+        <div className="cities__places-container cities__places-container--empty container">
+          <section className="cities__no-places">
+            <div className="cities__status-wrapper tabs__content">
+              <b className="cities__status">Page not found</b>
+              <p className="cities__status-description">
+                Click button to visit the main page
+              </p>
+              <Link to={AppPath.ROOT} className="button form__submit">
+                Go to the main page
+              </Link>
+            </div>
+          </section>
+          <div className="cities__right-section"></div>
         </div>
       </div>
-
     </main>
   );
 };
