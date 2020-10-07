2223ea1f64d0d3135e79dbdf28fe7b4981f64bfc
NinZZya <s.a.azarov@gmail.com>
2020-10-07 11:29:37 +0300

    Bug fix style

--- src/components/offer-header/offer-header.jsx
+++ src/components/offer-header/offer-header.jsx
@@ -15,7 +15,7 @@ const OfferHeader = ({title, isFavorite}) => {
         }
         type="button"
       >
-        <svg className="property__bookmark-icon" width="31" height="33">
+        <svg className="place-card__bookmark-icon" width="31" height="33">
           <use xlinkHref="#icon-bookmark"></use>
         </svg>
         <span className="visually-hidden">To bookmarks</span>
