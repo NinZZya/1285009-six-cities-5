ede5e460d5bd47276712e0b29920575925679e7a
NinZZya <s.a.azarov@gmail.com>
2020-10-07 11:10:00 +0300

    WIP. Add mocks to LoginPage and components

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import {AppPath} from '../../const';
+import * as Type from '../../types';
+import {AppPath, UserStatus} from '../../const';
 
-const Header = () => {
+
+const Header = ({userStatus = UserStatus.NO_AUTH, user = {}}) => {
+  const isAuth = userStatus === UserStatus.AUTH;
+
   return (
     <header className="header">
       <div className="container">
@@ -18,11 +22,15 @@ const Header = () => {
           <nav className="header__nav">
             <ul className="header__nav-list">
               <li className="header__nav-item user">
-                <a className="header__nav-link header__nav-link--profile" href="#">
+                <Link
+                  className="header__nav-link header__nav-link--profile"
+                  to={isAuth ? AppPath.FAVORITES : AppPath.LOGIN}>
                   <div className="header__avatar-wrapper user__avatar-wrapper">
                   </div>
-                  <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
-                </a>
+                  <span className="header__user-name user__name">
+                    {isAuth ? user.email : `Sign in`}
+                  </span>
+                </Link>
               </li>
             </ul>
           </nav>
@@ -31,5 +39,11 @@ const Header = () => {
     </header>
   );
 };
+
+Header.propTypes = {
+  userStatus: Type.USER_STATUS,
+  user: Type.USER,
+};
+
 
 export default Header;
--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,20 +1,45 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {Link} from 'react-router-dom';
 import LoginForm from '../../components/login-form/login-form';
+import {authUserAsync} from '../../redux/user/user-operations';
+import * as UserAction from '../../redux/user/user-actions';
+import * as OfferSelector from '../../redux/offers/offers-selectors';
+import * as UserSelector from '../../redux/user/user-selectors';
+import * as Type from '../../types';
+import {AppPath, CITIES, UserStatus} from '../../const';
 
 
-const Login = () => {
+const Login = (props) => {
+  const {
+    activeCityId,
+    userStatus,
+    error,
+    autUser,
+    changeUserStatus,
+  } = props;
+  const city = CITIES[activeCityId];
+  const cityPath = `${AppPath.CITY}/${activeCityId}`;
+
   return (
     <main className="page__main page__main--login">
       <div className="page__login-container container">
         <section className="login">
           <h1 className="login__title">Sign in</h1>
-          <LoginForm />
+          <LoginForm
+            onAuth={(authData) => {
+              changeUserStatus(UserStatus.RESPONSE);
+              autUser(authData);
+            }}
+            error={error}
+            userStatus={userStatus}
+          />
         </section>
         <section className="locations locations--login locations--current">
           <div className="locations__item">
-            <a className="locations__item-link" href="#">
-              <span>Amsterdam</span>
-            </a>
+            <Link to={cityPath} className="locations__item-link">
+              <span>{city.name}</span>
+            </Link>
           </div>
         </section>
       </div>
@@ -22,4 +47,29 @@ const Login = () => {
   );
 };
 
-export default Login;
+Login.propTypes = {
+  activeCityId: Type.ID,
+  userStatus: Type.USER_STATUS,
+  error: Type.USER_ERROR,
+  autUser: Type.FUNCTION,
+  changeUserStatus: Type.FUNCTION,
+};
+
+const mapStateToProps = (state) => ({
+  activeCityId: OfferSelector.getActiveCityId(state),
+  userStatus: UserSelector.getUserStatus(state),
+  error: UserSelector.getError(state),
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  autUser: (authData) => {
+    dispatch(authUserAsync(authData));
+  },
+  changeUserStatus: (userStatus) => {
+    dispatch(UserAction.changeUserStatus(userStatus));
+  },
+});
+
+
+export {Login};
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
