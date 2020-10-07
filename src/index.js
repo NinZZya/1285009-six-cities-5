ac2c8c056b52f4bba30ac3b223fdf0ecce5f74d2
NinZZya <s.a.azarov@gmail.com>
2020-10-07 11:05:01 +0300

    WIP. Add store to index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
 import {BrowserRouter as Router} from 'react-router-dom';
 import App from './app/app';
+import store from './store';
+import {loadOffersAsync} from './redux/offers/offers-operations';
 
+
+store.dispatch(loadOffersAsync());
 
 ReactDOM.render(
-    <Router>
-      <App />
-    </Router>,
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>,
     document.querySelector(`#root`)
 );
