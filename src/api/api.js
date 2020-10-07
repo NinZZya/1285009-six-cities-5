abecc9e2a30510d99c96fc3e8176fd00da305344
NinZZya <s.a.azarov@gmail.com>
2020-10-07 11:00:54 +0300

    WIP. Add API

---
+++ src/api/api.js
@@ -0,0 +1,46 @@
+import users from '../mocks/users';
+import generateOffers from '../mocks/generate-offers';
+
+const DELAY_MS = 500;
+const OFFERS_COUNT = 20;
+
+const OFFERS = generateOffers(OFFERS_COUNT);
+
+const delay = (ms) => new Promise((res) => setTimeout(res, ms));
+
+export default class Api {
+  static auth(authData) {
+    const {email, password} = authData;
+
+    return delay(DELAY_MS)
+      .then(() => {
+        const user = users.find((item) => item.email === email);
+
+        if (!user) {
+          throw new Error(`User not found`);
+        }
+
+        if (user && user.password === password) {
+          return {
+            id: user.id,
+            email: user.email,
+          };
+        }
+
+        throw new Error(`Bad authorization`);
+      });
+  }
+
+  static getOffers() {
+    return delay(DELAY_MS)
+      .then(() => OFFERS);
+  }
+
+  static adaptOffersToClient(offers) {
+    return offers.reduce((mapOffers, offer) => {
+      mapOffers[offer.id] = offer;
+      return mapOffers;
+    }, {});
+  }
+}
+
