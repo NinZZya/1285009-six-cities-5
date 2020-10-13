a10cd6196cd9f802fabf4531495a7e31fd6c8d80
NinZZya <s.a.azarov@gmail.com>
2020-10-13 21:25:00 +0300

    Use HOC with SortComponent

--- src/components/sort/sort.jsx
+++ src/components/sort/sort.jsx
@@ -1,76 +1,63 @@
-import React, {PureComponent} from 'react';
+import React from 'react';
+import withActiveFlag from '../../hocs/with-active-flag/with-active-flag';
 import * as Type from '../../types';
 
 
-class Sort extends PureComponent {
-  constructor(props) {
-    super(props);
+const Sort = (props) => {
+  const {
+    activeSort,
+    isActive,
+    onActiveChange,
+    sorts = [],
+    onSortClick = () => {},
+  } = props;
 
-    this.state = {
-      isOpened: false,
-    };
-  }
 
-  _toggleSort() {
-    const {isOpened} = this.state;
-    this.setState({isOpened: !isOpened});
-  }
-
-  render() {
-    const {
-      activeSort,
-      sorts = [],
-      onSortClick = () => {},
-    } = this.props;
-
-    const {isOpened} = this.state;
-
-    return (
-      <form className="places__sorting" action="#" method="get">
-        <span className="places__sorting-caption">Sort by</span>
-        <span
-          className="places__sorting-type"
-          tabIndex="0"
-          onClick={() => {
-            this._toggleSort();
-          }}
-        >
-          {activeSort}
-          <svg className="places__sorting-arrow" width="7" height="4">
-            <use xlinkHref="#icon-arrow-select"></use>
-          </svg>
-        </span>
-        <ul
-          className={`places__options places__options--custom ${isOpened ?
-            `places__options--opened` :
-            ``}`}
-        >
-          {sorts.map((sort, index) => (
-            <li
-              className={`places__option ${activeSort === sort ?
-                `places__option--active` :
-                ``}`}
-              tabIndex="0"
-              key={`sort-${index}`}
-              onClick={() => {
-                onSortClick(sort);
-                this._toggleSort();
-              }}
-            >
-              {sort}
-            </li>
-          ))}
-        </ul>
-      </form >
-    );
-  }
-}
+  return (
+    <form className="places__sorting" action="#" method="get">
+      <span className="places__sorting-caption">Sort by</span>
+      <span
+        className="places__sorting-type"
+        tabIndex="0"
+        onClick={onActiveChange}
+      >
+        {activeSort}
+        <svg className="places__sorting-arrow" width="7" height="4">
+          <use xlinkHref="#icon-arrow-select"></use>
+        </svg>
+      </span>
+      <ul
+        className={`places__options places__options--custom ${isActive ?
+          `places__options--opened` :
+          ``}`}
+      >
+        {sorts.map((sort, index) => (
+          <li
+            className={`places__option ${activeSort === sort ?
+              `places__option--active` :
+              ``}`}
+            tabIndex="0"
+            key={`sort-${index}`}
+            onClick={() => {
+              onSortClick(sort);
+              onActiveChange();
+            }}
+          >
+            {sort}
+          </li>
+        ))}
+      </ul>
+    </form >
+  );
+};
 
 Sort.propTypes = {
   activeSort: Type.SORT,
+  isActive: Type.FLAG,
+  onActiveChange: Type.FUNCTION,
   sorts: Type.SORTS,
   onSortClick: Type.FUNCTION,
 };
 
 
-export default Sort;
+export default withActiveFlag(Sort);
