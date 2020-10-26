0c59b24301e704705895b37d09db5448676865a6
NinZZya <s.a.azarov@gmail.com>
2020-10-26 23:27:26 +0300

    Add test for NoOffers. Code review

---
+++ src/components/no-offers/no-offers.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import NoOffers from './no-offers';
+import cities from '../../mocks/cities';
+
+
+const CITY = cities[1];
+
+it(`Should NoOffers render correctly`, () => {
+  const tree = renderer
+    .create(
+        <NoOffers city={CITY} />
+    )
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
