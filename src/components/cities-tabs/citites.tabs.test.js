fbb1f82cc25c7b49a5b5aa995465a8bc3a77cfff
NinZZya <s.a.azarov@gmail.com>
2020-10-26 23:23:07 +0300

    Add test for CitiesTabs. Code review

---
+++ src/components/cities-tabs/citites.tabs.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import {Router} from 'react-router-dom';
+import CitiesTabs from './cities-tabs';
+import history from '../../history';
+import cities from '../../mocks/cities';
+
+
+const ACTIVE_CITY_ID = 1;
+
+it(`Should CitiesTabs render correctly`, () => {
+  const tree = renderer
+    .create(
+        <Router history={history}>
+          <CitiesTabs
+            activeCityId={ACTIVE_CITY_ID}
+            cities={cities}
+          />
+        </Router>
+    )
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
