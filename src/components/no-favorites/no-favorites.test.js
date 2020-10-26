5c89d82a79429d6de1084272cf65f9efdf56500a
NinZZya <s.a.azarov@gmail.com>
2020-10-26 23:27:02 +0300

    Add test for NoFavorites. Code review

---
+++ src/components/no-favorites/no-favorites.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import NoFavorites from './no-favorites';
+
+
+it(`Should NoFavorites render correctly`, () => {
+  const tree = renderer
+    .create(
+        <NoFavorites />
+    )
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
