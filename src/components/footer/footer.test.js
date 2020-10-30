406b9a13081350eff8f79fb6b2d89b4b8673195e
NinZZya <s.a.azarov@gmail.com>
2020-10-30 17:08:31 +0300

    Fix test

--- src/components/footer/footer.test.js
+++ src/components/footer/footer.test.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import {Router} from 'react-router-dom';
 import renderer from 'react-test-renderer';
 import Footer from './footer';
+import history from '../../history';
 
 
 it(`Should Footer render correctly`, () => {
   const tree = renderer
     .create(
-        <Footer />
+        <Router history={history}>
+          <Footer />
+        </Router>
     )
     .toJSON();
 
